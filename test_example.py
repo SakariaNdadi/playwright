4e65bae07b6c3a4dbcd6db5ecd39b6072487ad21
SakariaNdadi <oipapi.ndadi@gmail.com>
2024-12-13 15:38:57 +0200

    code: make test_has_title pass

--- test_example.py
+++ test_example.py
@@ -5,7 +5,7 @@ from playwright.sync_api import Page, expect
 # this test will fail because there is no title named "niks" on the google homepage
 def test_has_title(page: Page) -> None:
     page.goto("https://google.com")
-    expect(page).to_have_title(re.compile("niks"))
+    expect(page).to_have_title(re.compile("Google"))
 
 
 def test_get_feeling_lucky_link(page: Page) -> None:
