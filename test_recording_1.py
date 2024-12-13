ba9881eb827a9bcfbd30fc27059c068fca49c172
SakariaNdadi <oipapi.ndadi@gmail.com>
2024-12-13 15:32:48 +0200

    code: change test_recording_1 to run in headless mode.

--- test_recording_1.py
+++ test_recording_1.py
@@ -3,8 +3,7 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def test_run(playwright: Playwright) -> None:
-    browser = playwright.firefox.launch(headless=False)
-    # browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://demo.playwright.dev/todomvc/#/")
